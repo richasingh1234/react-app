d63c2efdd12193130d6fb3cc8b53268b18bd4d13
richasingh1234 <rrichasingh84@gmail.com>
2022-04-14 10:32:46 +0530

    api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React,{  Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    console.log("inside handleGetJson");
+
+    fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then(res => {
+        if (res && res.data) {
+          console.log("hii");
+          console.log(res.data);
+          this.setState({
+            users: [...this.state.users,...res.data]
+          })
+        }
+      });
+  }
+
+  renderUsers()
+  {
+    console.log(this.state.users.length);
+    if(this.state.users.length<=0)
+    {
+      return <div>...loading</div>;
+    }else{
+      return this.state.users.map((val,key)=>
+      {
+        return <div key={key}>{val.name} | {val.age}</div>
+      }
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.renderUsers()}
+      </div>
+      );
+  }
 }
 
 export default App;
